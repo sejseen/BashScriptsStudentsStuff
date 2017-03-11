481e72e6e689e2eda6681953da17baced7224631
Konrad Tyma <sejseen@gmail.com>
2017-03-11 11:06:56 +0100

    Lab2 Subject:Symlinks and commends.txt init

---
+++ lab2/test_zad1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# ============================
+# W zadanym katalogu pliki reguarne, ktore sa wzgledem siebie twardymi dowiazaniami
+# przerobić na wiązania symboliczne (jeden zostawić - pozostałe symboliczne - ze ściezka względną)
+# ============================
+# example:
+# ./zad1 .
+# ============================
+
+if [ $# -ne 1 -o ! -d $1 ]; then
+    echo Błędny katalog
+    exit
+fi
+
+for file in $1/*; do
+    if [ ! -f $file ]; then
+        continue
+    fi
+    idnode=$(ls -i $1/$file | awk '{ print $1 }')
+    for file2 in $1/*; do
+        if [ ! -f $file2 ]; then
+            continue
+        fi
+        idnode2=$(ls -i $1/$file2 | awk '{ print $1 }')
+        if [ $idnode = $idnode2 -a $1/$file != $1/$file2 ]; then
+            echo $file $file2 $idnode $idnode2
+            rm $file2
+            ln -sf $file $file2
+        fi
+       
+    done
+done
---
+++ lab2/test_zad2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# ============================
+# Kopia zadango katalogu w zadanym miejscu tak, aby wszytkie dowiazania
+# symbloiczne w kopii wskazywały na te same obiekty co w katalogu pierwotnym
+# ============================
+# example:
+# ./zad2 .
+# ============================
+
+if [ $# -ne 2 ]; then
+    echo Błędna ilość argumetów
+    exit
+fi
+
+if [ ! -d $1 ]; then
+    echo Błędny katalog pierwotny
+    exit
+fi
+
+if [ ! -d $2 ]; then
+    echo Błędny katalog docelowy
+    exit
+fi
+
+for file in $1/*; do
+    if [ -h $file ]; then
+        echo copied link $file
+         filepath=$(readlink -f $file)
+           ln -sf $filepath $2/$file
+    elif [ -f $file ]; then
+        echo coppied $file
+        cp $file $2/$file
+    fi
+done
